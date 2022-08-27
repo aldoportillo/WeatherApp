7ccaf2ccf15d2c5216e9e4f309dd2e801e2d4070
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-27 10:16:29 -0500

    Write Async await function and import API

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("hello world")
+
+async function getWeather (location) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=6b90e7ed6704a9984fc25f85e2580025`, {mode: 'cors'})
+        const weatherData = await response.json();
+        console.log(weatherData)
+    } catch (error){
+        console.log("Location not found")
+    }
+}
+
